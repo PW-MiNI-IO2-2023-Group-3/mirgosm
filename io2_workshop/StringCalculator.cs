67420bce04a0e9934c4f9a258ca00e3ea2c6f282
Mieszko <mirgosmieszko@gmail.com>
2023-02-27 09:21:33 +0100

    Add project files.

---
+++ io2_workshop/StringCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io2_workshop
+{
+    public class StringCalculator
+    {
+        public static int SumString(string str)
+        {
+            if (str == String.Empty)
+            {
+                return 0;
+            }
+
+
+            List<char> sums = new List<char>(){',','\n'};
+            if (str.Length > 4)
+            {
+                if (str.Substring(0,2) =="//")
+                {
+                    sums.Add(str[2]);
+                    str = str.Substring(4);
+                }
+            }
+            
+            int retval=0;
+
+            
+            string[] arr = str.Split(sums.ToArray());
+            
+            foreach (string s in arr)
+            {
+                if (int.Parse(s)<0)
+                {
+                    throw new ArgumentException();
+                }
+
+                if (int.Parse(s) > 1000)
+                {
+                    retval += 0;
+                }
+                else
+                {
+                    retval += int.Parse(s);
+                }
+                
+            }
+
+            return retval;
+            //return Int32.Parse(str);
+        }
+    }
+}
---
+++ io2_workshop_tests/StringCalculatorTests.cs
@@ -0,0 +1,82 @@
+using io2_workshop;
+
+namespace io2_workshop_tests
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void EmptyStringReturnsZero()
+        {
+            int result = StringCalculator.SumString("");
+            Assert.Equal(0,result);
+        }
+
+        [Theory]
+        [InlineData("12",12)]
+        [InlineData("13",13)]
+        [InlineData("0",0)]
+        public void SingleNumberReturnsValue(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue,result);
+        }
+
+        [Theory]
+        [InlineData("12,13",25)]
+        [InlineData("12,4",20)]
+        [InlineData("0,3",3)]
+        public void CommaSeparatedReturnSum(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue,result);
+        }
+
+        [Theory]
+        [InlineData("12\n13",25)]
+        [InlineData("12\n4",16)]
+        [InlineData("0\n3",3)]
+        public void NewlineSeparatedReturnSum(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue,result);
+        }
+
+        [Theory]
+        [InlineData("1,12\n13",26)]
+        [InlineData("1\n4,2",7)]
+        public void NewlineOrSplSeparatedReturnSum(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue,result);
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("-1,2\n3")]
+        [InlineData("-1,-2\n3")]
+        public void NegtiveReturnArgException(string str)
+        {
+            Assert.Throws<ArgumentException>(() => { StringCalculator.SumString(str); });
+        }
+
+        [Theory]
+        [InlineData("1001,12\n13",25)]
+        [InlineData("1\n4,2000",5)]
+        [InlineData("2000",0)]
+        public void GreaterThanThousandReturnSum(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue,result);
+        }
+
+        [Theory]
+        [InlineData("//a\n1a12a13",26)]
+        [InlineData("//$\n1\n4$2000",5)]
+        [InlineData("//p\n2000",0)]
+        public void CustomSeparator(string str, int expectedValue)
+        {
+            int result = StringCalculator.SumString(str);
+            Assert.Equal(expectedValue,result);
+        }
+    }
+}
